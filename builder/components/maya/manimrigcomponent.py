9002b7f0f2cb354bf5882cdf6419a438ed1fbdbc
Anton Stattin <anton.stattin@gmail.com>
2018-09-19 22:44:08 +0200

    added shape export for controllers etc

--- builder/components/maya/manimrigcomponent.py
+++ builder/components/maya/manimrigcomponent.py
@@ -79,7 +79,7 @@ class RigGuide(rigdata.RigNode):
                 fnc(oGuide)
 
                 for scale in ['.sx', '.sy', '.sz']:
-                    cmds.setAttr(oGuide + scale, 0.8)
+                    cmds.setAttr(oGuide + scale, 0.3)
                 cmds.makeIdentity(oGuide, a=True)
 
                 position_data.append(oGuide)
